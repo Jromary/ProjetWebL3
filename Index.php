b61aa8dd9c03f4ff946e0a0ffb99f562ff366fe7
karim <karim.abdi9@etu.univ-lorraine.fr>
2018-11-12 16:03:02 +0100

    test1
    test1

--- Index.php
+++ Index.php
@@ -1,16 +1,44 @@
 <html>
 <?php
-	include Donnees.inc.php;
+	include "Donnees.inc.php";
 ?>
+
+<script type="text/javascript">
+	function seconde_category( ){
+			/*foreach($categorie as $H => $hs){
+				var option = document.createElement("option");
+				option.text=$hs
+				document.getElementById("sousCategorie").appendChild("<option value='".$H."' onchange='seconde_category(".$H.")' >".$H."</option>");*/
+				alert(document.getElementById("superClass").options[document.getElementById("superClass").selectedIndex].text);
+				//alert("Hello! I am an alert box!!");
+
+                      
+	
+	}
+</script>
+
 <head>
 	
 </head>
 
 <body>
+	<select id="superClass" onchange='seconde_category()' >
+
 	<?php
-	foreach($Recettes as $Recette){
-		echo $Recette;
+	foreach($Hierarchie as $H => $hs){
+		echo "<option value='".$H."'  >".$H."</option>";
+			
+
+		/**foreach ($Recette as $key => $value) {
+			echo "<option value='".."' >".."</option>";
+			echo $key ." ".$value."<br/>";
+		}**/
 	}
+
 	?>
+	</select>
+
+		<select id="sousCategorie" ></select>
+      
 </body>
 </html>
